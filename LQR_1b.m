7a56b7c2421b894bd88333cbe5d9dd7611e2a583
Midhun T Augustine <96012490+MIDHUNTA30@users.noreply.github.com>
2021-12-15 12:14:42 +0530

    Update LQR_1b.m

--- LQR_1b.m
+++ LQR_1b.m
@@ -3,33 +3,30 @@ function main
 clear all;
 close all;
 % initialize system and simulation parameters
-global A B   
-N=50;
-Qf=[1 0;0 1];
-Q=[1 0;0 1]; J0=0;
-R=1;s=1; n=2;
-x0 = [10;5]; 
 A=[0.5 0;-1 1.5];
 B=[0.5;0.1];
+N=5;n=2;m=1;s=1;J0=0;
+Qf=[1 0;0 1];Q=[1 0;0 1];R=1;
+x0 = [10;5];
+ 
 
 
 % Calling the LQR function
-[K0,P0] = LQR(A,B,Qf,Q,R,N,n);
+[K0,P0] = LQR(A,B,Qf,Q,R,N,n,m);
 
 % Initializing the vectors and matrices
-x = zeros(2,N+1);
-x(:,1)=x0;
-u = zeros(1,N);
+x = zeros(n,N+1);x(:,1)=x0;
+u = zeros(m,N);
   
 % Simulating the system with LQR
 for j=1:N   
-      K=K0(j,:);
-      %K=[2.735 -2.747];
-      u(j)=-K*x(:,j);
-      x(:,j+1)=A*x(:,j)+B*u(1,j);
-      J0=J0+x(:,j)'*Q*x(:,j)+u(j)'*R*u(j);
-   end      
-     J1=J0+ x(:,N+1)'*Qf*x(:,N+1)     
+   K=K0(j,:);
+   %K=[2.735 -2.747];
+   u(j)=-K*x(:,j);
+   x(:,j+1)=A*x(:,j)+B*u(:,j);
+   J0=J0+x(:,j)'*Q*x(:,j)+u(:,j)'*R*u(:,j);
+end      
+     J1=J0+ x(:,N+1)'*Qf*x(:,N+1);     
    
 % Plotting the responces     
 figure(1)
@@ -39,7 +36,7 @@ plot(time,x(1,:),'k.-','LineWidth',1)
 hold on
 plot(time,x(2,:),'r.-','LineWidth',1) 
 legend('$x_{1}$','$x_{2}$','Interpreter','latex');
-axis([0 50 -10 10])
+axis([0 5 -10 10])
 xlabel('k','Interpreter','latex');ylabel('$\textbf{x}_{k}$','Interpreter','latex');
 grid on
 ax = gca;
@@ -47,7 +44,7 @@ ax.GridAlpha = 1
 ax.GridLineStyle = ':'
 subplot(4,1,2)
 plot(time(1:end-1),u,'k.-','LineWidth',1)
-axis([0 50 -13 2])
+axis([0 5 -13 2])
 xlabel('k','Interpreter','latex');ylabel('$\textbf{u}_{k}$','Interpreter','latex');
 grid on
 ax = gca;
@@ -58,7 +55,7 @@ plot(time(1:end-1),K0(:,1),'k.-','LineWidth',1)
 hold on
 plot(time(1:end-1),K0(:,2),'r.-','LineWidth',1) 
 legend('$K_{1}$','$K_{2}$','Interpreter','latex');
-axis([0 50 -3 3])
+axis([0 5 -3 3])
 xlabel('k','Interpreter','latex');ylabel('$\textbf{K}_{k}$','Interpreter','latex');
 grid on
 ax = gca;
@@ -67,30 +64,27 @@ ax.GridLineStyle = ':'
 subplot(4,1,4)
 plot(time,P0(:,1),'k.-','LineWidth',1) 
 hold on
-plot(time,P0(:,4),'r.-','LineWidth',1)
+plot(time,P0(:,2),'r.-','LineWidth',1)
 legend('$P_{11}$','$P_{22}$','Interpreter','latex');
-%axis([0 50 -10 10])
+axis([0 5 0 20])
 xlabel('k','Interpreter','latex');ylabel('$\textbf{P}_{k}$','Interpreter','latex');
 grid on
 ax = gca;
 ax.GridAlpha = 1
 ax.GridLineStyle = ':'
-print -dsvg fig1
+print -dsvg fig2
 end
 
-
 % LQR function
-function [K0,P0] = LQR(A,B,Qf,Q,R,N,n)
+function [K0,P0] = LQR(A,B,Qf,Q,R,N,n,m)
 P=Qf;
-P0(N+1,1:2)=P(1,1:2);
-P0(N+1,3:4)=P(2,1:2);
-    for k=N-1:-1:0
-                                    
+P0(N+1,:)=diag(P);
+
+  for k=N-1:-1:0                                    
      K = inv(R +B'* P*B)*(B'* P*A);
      P = Q + K'*R*K+(A-B*K)'* P*(A-B*K); 
      K0(k+1,:)=K;
-     P0(k+1,1:2)=P(1,1:2);
-     P0(k+1,3:4)=P(2,1:2);
-    end   
+     P0(k+1,:)=diag(P);
+  end   
 
 end
